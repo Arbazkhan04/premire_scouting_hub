cf42ee3a1df5fb1171365415131d8201dc182ccb
Mohsin Zaheer <msnoay@hotmail.com>
2025-02-12 12:11:03 +0500

    add

--- backend/soccer/controllers/teamStatistics.controller.js
+++ backend/soccer/controllers/teamStatistics.controller.js
@@ -1,5 +1,8 @@
-const { processTeamStatistics } = require("../services/teamStatistics.service")
-const CustomError = require("../../utils/customError")
+const {
+  processTeamStatistics,
+  fetchandSaveAllTeamsStatistics,
+} = require("../services/teamStatistics.service");
+const CustomError = require("../../utils/customError");
 const responseHandler = require("../../utils/responseHandler"); // Ensure this utility is correctly imported
 
 /**
@@ -18,8 +21,38 @@ const processTeamStatisticsController = async (req, res, next) => {
     responseHandler(res, 200, "Team statistics processed successfully", result);
   } catch (error) {
     console.error("Error in processTeamStatisticsController:", error.message);
-    next(error instanceof CustomError ? error : new CustomError(error.message, 500));
+    next(
+      error instanceof CustomError ? error : new CustomError(error.message, 500)
+    );
   }
 };
 
-module.exports = { processTeamStatisticsController };
+/**
+ * Controller to process team statistics
+ */
+const fetchandSaveAllTeamsStatisticsController = async (req, res, next) => {
+  try {
+    const { leagueId } = req.query;
+
+    if (!leagueId) {
+      throw new CustomError("League ID is required", 400);
+    }
+
+    const result = await fetchandSaveAllTeamsStatistics(leagueId);
+
+    responseHandler(res, 200, result.message, result);
+  } catch (error) {
+    console.error(
+      "Error in fetchandSaveAllTeamsStatistics controller:",
+      error.message
+    );
+    next(
+      error instanceof CustomError ? error : new CustomError(error.message, 500)
+    );
+  }
+};
+
+module.exports = {
+  processTeamStatisticsController,
+  fetchandSaveAllTeamsStatisticsController,
+};
