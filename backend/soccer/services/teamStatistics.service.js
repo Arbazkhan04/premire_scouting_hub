69bd5e0267924e5009ff1930b79fe6079d25fd8e
Mohsin Zaheer <msnoay@hotmail.com>
2025-02-19 11:29:32 +0500

    updated

--- backend/soccer/services/teamStatistics.service.js
+++ backend/soccer/services/teamStatistics.service.js
@@ -59,7 +59,11 @@ const fetchTeamStatistics = async (leagueId, season, teamId) => {
     // Call the apiRequest method instead of axios directly
     const responseData = await apiRequest(
       "https://api-football-v1.p.rapidapi.com/v3/teams/statistics",
-      params
+      params,
+      {
+        "x-rapidapi-key": process.env.SOCCER_API_KEY,
+        "x-rapidapi-host": "api-football-v1.p.rapidapi.com",
+      }
     );
 
     // Validate response structure
